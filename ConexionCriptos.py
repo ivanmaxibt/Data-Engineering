c023b5e0340237880028b662069d8c0c7ba638f7
ivanmaxibt <107760930+ivanmaxibt@users.noreply.github.com>
2024-01-05 21:45:16 -0300

    Add files via upload

--- ConexionCriptos.py
+++ ConexionCriptos.py
@@ -1,33 +1,47 @@
-# Importo las librerías 
-import pandas as pd
-import pyodbc
+# Importo las librerías
+import os
+from dotenv import load_dotenv
+import psycopg2
+import boto3
 
-# Valores de la configuración de SQL Server
-server = 'LAPTOP-AFF2SUSG\SQLEXPRESS'
-database = 'Criptomonedas'
+# Cargo las variables de entorno desde el archivo .env
+load_dotenv("C:\\Users\\Iván\\Downloads\\Entregable\\rs.env")
 
-# Establezco la cadena de conexión
-connection = f'DRIVER=SQL Server;SERVER={server};DATABASE={database};'
-cnxn = pyodbc.connect(connection)
+# Conexión a Redshift
+conn = psycopg2.connect(
+    host=os.getenv("Host"),
+    port=os.getenv("port"),
+    dbname=os.getenv("Database"),
+    user=os.getenv("Usuario"),
+    password=os.getenv("Contraseña"),
+)
 
-# Leer el archivo CSV en un DataFrame de pandas
-df = pd.read_csv('C:\\Users\\Iván\\Downloads\\criptos.csv')
+# Carga el archivo CSV en el bucket de S3
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('criptos')
+bucket.upload_file('C:\\Users\\Iván\\Downloads\\criptos.csv', 'criptos.csv')
 
-# Inserción de datos en la tabla
-cursor = cnxn.cursor()
-for index, row in df.iterrows():
-    cursor.execute('INSERT INTO Criptomonedas (Nombre, Ranking, Símbolo, Suministro, [Máximo Suministro], [Capitalización de Mercado USD], [Volumen USD], [Precio USD], [Cambio Porcentual 24h], [Precio PPV 24h], [Fuente de Datos]) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                   row['name'], row['rank'], row['symbol'], row['supply'], row['maxSupply'], row['marketCapUsd'], row['volumeUsd24Hr'], row['priceUsd'], row['changePercent24Hr'], row['vwap24Hr'], row['explorer'])
-    cnxn.commit()
+# Crear tabla staging
+with conn.cursor() as cur:
+    cur.execute('CREATE TABLE tabla_staging (Nombre VARCHAR(255), Ranking INT, Simbolo VARCHAR(255), Suministro FLOAT, MS FLOAT, CDMUSD FLOAT, VOLUMENUSD FLOAT, PRECIOUSD FLOAT, CP24H FLOAT, PPPV24H FLOAT, FDD VARCHAR(255));')
 
-# Consulta SQL para obtener los datos de la tabla
-sql_query = "SELECT * FROM Criptomonedas"
+# Cargar datos en la tabla staging
+with conn.cursor() as cur:
+    cur.execute("COPY tabla_staging FROM 's3://criptos/criptos.csv' DELIMITER ',' CSV HEADER;")
 
-# Carga de datos desde la base de datos
-df = pd.read_sql(sql_query, cnxn)
+# Realizar merge con la tabla destino
+with conn.cursor() as cur:
+    cur.execute('''
+        MERGE INTO tabla_destino AS t
+        USING tabla_staging AS s
+        ON t.columna_clave = s.columna_clave
+        WHEN MATCHED THEN
+            UPDATE SET t.columna1 = s.columna1, t.columna2 = s.columna2, t.columna1 = s.columna1, t.columna2 = s.columna2, t.columna3 = s.columna3, t.columna4 = s.columna4, t.columna5 = s.columna5, t.columna6 = s.columna6, t.columna7 = s.columna7, t.columna8 = s.columna8, t.columna9 = s.columna9, t.columna10 = s.columna10, t.columna11 = s.columna11
+        WHEN NOT MATCHED THEN
+            INSERT (columna_clave, columna1, columna2, columna3, columna4, columna5, columna6, columna7, columna8, columna9, columna10, columna11)
+            VALUES (s.columna_clave, s.columna1, s.columna2, s.columna3, s.columna4, s.columna5, s.columna6, s.columna7, s.columna8, s.columna9, s.columna10, s.columna11));
+    ''')
 
-# Cierre de la conexión
-cnxn.close()
-
-# Guardar los datos en un archivo CSV
-df.to_csv('C:\\Users\\Iván\\Downloads\\Criptomonedas.csv', index=False)
+# Eliminar tabla staging
+with conn.cursor() as cur:
+    cur.execute('DROP TABLE tabla_staging;')
